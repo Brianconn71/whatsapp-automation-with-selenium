48fa6d5c10e4ecd258e548934fffb88d93848c0d
Brian Connolly <Brian.connolly71@gmail.com>
2022-05-22 21:40:16 +0100

    initial commit

---
+++ dang.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+path = 'C:\Users\User\Downloads\chromedriver_win32'
+website = 'https://web.whatsapp.com/'
